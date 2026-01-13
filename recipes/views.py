b11738bc8769a95e70ff1e0795f29a260b415ba2
natancelidonio <natan.c.f.adm@gmail.com>
2026-01-13 11:11:01 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={'name': 'Exemplo',})
-
-
-def contato(request):
-    return HttpResponse('contato 1')
-
-
-def sobre(request):
-    return HttpResponse('sobre 1')
