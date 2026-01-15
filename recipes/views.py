8ad7596af615b2003f1f5dbeab4ec592264ce71f
natancelidonio <natan.c.f.adm@gmail.com>
2026-01-15 16:49:42 -0300

    Criando a header, partial pra head e usando include no template

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Exemplo',})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Exemplo',})
